544a000cae513d20f91919ba98612be437ab7d14
sacul2345 <lukesteinbrenner@gmail.com>
2024-07-09 01:20:12 -0500

    added space dust

---
+++ scenes/spaceDust.gd
@@ -0,0 +1,15 @@
+extends CPUParticles3D
+
+@export var shipRigidBody : RigidBody3D
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(shipRigidBody != null):
+		if(shipRigidBody.linear_velocity.length() > 40):
+			emitting = true
+		else:
+			emitting = false
