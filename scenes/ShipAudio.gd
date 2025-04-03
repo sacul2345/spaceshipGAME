314fd7c165e71470cca07b92fe6fce5c02519c4d
sacul2345 <lukesteinbrenner@gmail.com>
2025-04-03 13:51:07 -0500

    added tracers and all that

--- scenes/ShipAudio.gd
+++ scenes/ShipAudio.gd
@@ -5,10 +5,9 @@ extends AudioStreamPlayer3D
 @export var engineWah : AudioStreamPlayer3D
 
 func _physics_process(_delta: float) -> void:
-	print("test")
 	if(shipRigidBody != null && engineAmbient != null && engineWah != null):
 		pitch_scale = 1 + shipRigidBody.linear_velocity.length()/150
-		engineAmbient.volume_db = (-5 - shipRigidBody.linear_velocity.length()/5) - 10
+		engineAmbient.volume_db = (-5 - shipRigidBody.linear_velocity.length()/5)
 		if(shipRigidBody.linear_velocity.length() > 40):
 			engineWah.volume_db = -30 + (shipRigidBody.linear_velocity.length()/4)
 			engineWah.pitch_scale = shipRigidBody.linear_velocity.length()/150
--- scenes/Weapons.gd
+++ scenes/Weapons.gd
@@ -2,8 +2,12 @@ extends Node3D
 
 @export var gunTimer : Timer
 @export var gunShot : AudioStreamPlayer3D
+@export var muzzleLight : OmniLight3D
+
+var BULLETSPEED : float = 10000
 
 var explosionInstance = preload("res://scenes/explosion.tscn")
+var tracerInstance = preload("res://scenes/tracer.tscn")
 
 var weaponsFiring : bool
 var gunRPMTimer = .1
@@ -17,10 +21,6 @@ var gun2 : RayCast3D
 
 var gun1Flash : Sprite3D
 var gun2Flash : Sprite3D
-
-
-func _on_view_ray_cast_tree_entered():
-	viewCast = $"../ThirdPersonCam/ViewRayCast"
 	
 func _on_gun_1_tree_entered():
 	gun1 = $Gun1
@@ -52,21 +52,53 @@ func muzzleFlash():
 	if(gun1Flash.visible == false):
 		gun1Flash.visible = true
 		gun2Flash.visible = true
+		muzzleLight.visible = true
 		await get_tree().create_timer(.05).timeout
+		muzzleLight.visible = false
 		gun1Flash.visible = false
 		gun2Flash.visible = false
 
+#hitscan funcion is BADDD and needs to be RE-WRITTEN, will do that someday... ITS LAGGING!!
+
 func hitScan():
 	if(gun1.is_colliding()):
-		var explosion : Node3D = explosionInstance.instantiate()
-		get_tree().current_scene.add_child(explosion)
 		var explosionPos = gun1.get_collision_point()
-		explosion.position = explosionPos
-	if(gun2.is_colliding()):
-		var explosion : Node3D = explosionInstance.instantiate()
-		get_tree().current_scene.add_child(explosion)
-		var explosionPos = gun2.get_collision_point()
-		explosion.position = explosionPos
-
-	
+		var gunDisatnce = explosionPos.distance_to(position)
 		
+		drawTracer(gun1.global_position,gun1.global_rotation_degrees,explosionPos)
+		
+		var explosion : Node3D = explosionInstance.instantiate()
+		await get_tree().create_timer(gunDisatnce/BULLETSPEED).timeout
+		get_tree().current_scene.add_child(explosion)
+		explosion.position = explosionPos
+	else:
+		drawTracer(gun1.global_position,gun1.global_rotation_degrees,get_ray_end_point(gun1,-1000))
+		
+	if(gun2.is_colliding()):
+		var explosionPos = gun2.get_collision_point()
+		var gunDisatnce = explosionPos.distance_to(position)
+		
+		drawTracer(gun2.global_position,gun2.global_rotation_degrees,explosionPos)
+		
+		var explosion : Node3D = explosionInstance.instantiate()
+		await get_tree().create_timer(gunDisatnce/BULLETSPEED).timeout
+		get_tree().current_scene.add_child(explosion)
+		explosion.position = explosionPos
+	else:
+		drawTracer(gun2.global_position,gun2.global_rotation_degrees,get_ray_end_point(gun2,-1000))
+
+func drawTracer(pos : Vector3, rot : Vector3, targetPos : Vector3):
+	var tracer : Node3D = tracerInstance.instantiate()
+	get_tree().current_scene.add_child(tracer)
+	tracer.position = pos
+	tracer.rotation_degrees = rot
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property(tracer, "position", targetPos, targetPos.distance_to(pos)/(BULLETSPEED/10.0))
+	tween.tween_callback(tracer.queue_free)
+	#tween.connect("tween_all_completed", queue_free, 0)
+		
+func get_ray_end_point(raycast : RayCast3D, distance : float) -> Vector3:
+	var start = raycast.global_position
+	var direction = raycast.global_transform.basis.z.normalized()
+	return start + direction * distance
