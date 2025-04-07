b864c000981b0802f29b855d22dd7c8c54864e70
lucas <lukesteinbrenner@gmail.com>
2025-04-07 00:37:24 -0500

    changed up menu a lil and fixed some things and also something like a little more saturation...

--- models/spaceshiptest_2.gd
+++ models/spaceshiptest_2.gd
@@ -1,8 +1,12 @@
 extends Node3D
 var time : float
+@export var world : WorldEnvironment
 
 func _process(delta: float) -> void:
 	
-	position.y += sin(.4*time) * delta
-	rotation.x = 2*sin(.4*time) * delta
-	time+=.01
+	position.y += .5*sin(.4*time) * delta
+	rotation.x = sin(1*time) * delta
+	time+= 2*delta
+
+	#world.environment.sky_rotation.y -= 0.0000005
+	#world.environment.sky_custom_fov += 0.0005
