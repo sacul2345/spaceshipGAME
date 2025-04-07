f486c19909cffdcb5165e3496c0b2488e78ea567
sacul2345 <lukesteinbrenner@gmail.com>
2025-04-07 13:40:16 -0500

    started work on multuplayer menu elements

--- scripts/Mainmenu.gd
+++ scripts/Mainmenu.gd
@@ -1,7 +1,17 @@
 extends Control
 
 @export var freeRoamButton : Button
+
+@export var multiplayerButton : Button
+@export var multiplayerWindow : Window
+
+@export var settingsButton : Button
+@export var settingsWindow : Window
+
+
+
 @export var menuMusic : AudioStreamPlayer
+
 var freeRoamScene = preload("res://scenes/game_area.tscn")
 var playMusic : bool = true
 
@@ -14,3 +24,16 @@ func _on_free_roam_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game_area.tscn")
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	Hud.visible = true
+
+func _on_window_close_requested() -> void:
+	multiplayerWindow.hide()
+func _on_multiplayer_button_pressed() -> void:
+	multiplayerWindow.show()
+
+
+func _on_settings_button_pressed() -> void:
+	settingsWindow.show()
+
+
+func _on_settings_window_close_requested() -> void:
+	settingsWindow.hide()
